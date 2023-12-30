4e752f2c6b2832cfe794a045cb7c7a3b19a6b47b
Dragos Barbuta <barbuta.dragos12@gmail.com>
2023-12-30 19:10:23 +0100

    Attach the Store to the window object, which is now a global store, and create a separate Menu service for menu data.

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
+import Store from "./services/Store.js";
+import API from "./services/API.js";
+import { loadData } from "./services/Menu.js";
+
+window.app = {};
+app.store = Store;
+
 // It's better to wait for the event for manipulation
-window.addEventListener("DOMContentLoaded", () => {
- 
-})
+window.addEventListener("DOMContentLoaded", async () => {
+  loadData();
+});

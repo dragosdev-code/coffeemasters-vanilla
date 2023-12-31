d989d33602b26b7488630b5369961baa1d7086b8
Dragos Barbuta <barbuta.dragos12@gmail.com>
2023-12-31 15:45:10 +0100

    dynamic routing and event listener for URL changes

--- services/Router.js
+++ services/Router.js
@@ -9,6 +9,12 @@ const Router = {
       });
     });
 
+    // Event Handler for URL changes
+    window.addEventListener("popstate", (event) => {
+      Router.go(event.state.route, false);
+      console.log({state: event.state})
+    });
+
     // Check the initial URL
     Router.go(location.pathname);
   },
@@ -17,6 +23,35 @@ const Router = {
 
     if (addToHistory) {
       history.pushState({ route }, "", route);
+    }
+
+    let pageElement = null;
+
+    switch (route) {
+      case "/":
+        pageElement = document.createElement("h1");
+        pageElement.textContent = "Menu";
+        break;
+      case "/order":
+        pageElement = document.createElement("h1");
+        pageElement.textContent = "Your Order";
+        break;
+      default:
+        if (route.startsWith("/product-")) {
+          pageElement = document.createElement("h1");
+          pageElement.textContent = "Details";
+          const paramId = route.substring(route.lastIndexOf("-") + 1);
+          pageElement.dataset.id = paramId;
+        }
+        break;
+    }
+
+    if (pageElement) {
+      // document.querySelector("main").children[0].remove()
+      const cache = document.querySelector("main");
+      (cache.innerHTML = ""), cache.appendChild(pageElement);
+      window.scrollX = 0;
+      window.scrollY = 0;
     }
   },
 };

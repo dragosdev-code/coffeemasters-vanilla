edf5c31c30e2b0cb25f42ada2fd227d5bf943a42
Dragos Barbuta <barbuta.dragos12@gmail.com>
2024-01-02 18:31:05 +0100

    made the DetailsPage component

--- services/Menu.js
+++ services/Menu.js
@@ -2,16 +2,18 @@ import API from "./API.js";
 
 export async function loadData() {
   app.store.menu = await API.fetchMenu();
+  console.log(app.store.menu);
 }
 
 export async function getProductById(id) {
   if (app.store.menu === null) {
     await loadData();
   }
-  for (let c of app.store.menu) {
-    for (let p of c.products) {
-      if (p.id === id) {
-        return p;
+
+  for (let item of app.store.menu) {
+    for (let product of item.products) {
+      if (product.id == id) {
+        return product;
       }
     }
   }
--- services/Router.js
+++ services/Router.js
@@ -37,9 +37,8 @@ const Router = {
       default:
         if (route.startsWith("/product-")) {
           pageElement = document.createElement("details-page");
-          pageElement.textContent = "Details";
           const paramId = route.substring(route.lastIndexOf("-") + 1);
-          pageElement.dataset.id = paramId;
+          pageElement.dataset.productId = paramId;
         }
         break;
     }
